977c6afb79abcc460aba891dbf268cec19e75d41
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-08-29 14:51:02 +0200

    Dodanie przykładu przetwarzania pliku CSV

---
+++ ProcessFile/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessFile
+{
+    internal class Item
+    {
+        public string barCode;
+        public string name;
+        public string color;
+        public decimal price;
+    }
+}
---
+++ ProcessFile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace ProcessFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Process File!");
+
+            const string filename = "Items.csv";
+            const char separator = ',';
+
+            CultureInfo culture = new CultureInfo("en");
+
+            // Tablica
+            // string content = File.ReadAllText(filename);
+
+            // Załadowanie listy produktów
+            string[] lines = File.ReadAllLines(filename);
+
+            Item[] items = new Item[lines.Length - 1];
+
+            int i = 0;
+
+            // Iteracja po elementach tablicy
+            foreach (string line in lines.Skip(1))
+            {
+                // Rozdzielenie tekstu na tablicę
+                string[] columns = line.Split(separator);
+
+                Item item = new Item();
+                item.barCode = columns[0];
+                item.name = columns[1];
+                item.color = columns[2];
+                item.price = decimal.Parse(columns[3], culture);
+
+                items[i++] = item;                
+            }
+
+            // Wyświetlenie listy produktów
+            for(i = 0; i < items.Length; i++)
+            {
+                Item item = items[i];
+
+                Console.WriteLine($"{item.barCode} {item.name} {item.color} {item.price}");
+            }
+
+        }
+    }
+}
